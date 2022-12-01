3cfc9ee84f389b425fbc07ab7fffbea1422040db
Jawad Kalia <>
2022-12-01 16:35:33 -0600

    Update main.rs

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -10,13 +10,10 @@ fn main() {
 fn parse_into_vector_of_sums() -> Vec<u32> {
     include_str!("../input.txt")
         .split("\n\n")
-        .collect::<Vec<&str>>()
-        .iter()
+        
         .map(|n| {
             n.lines()
                 .map(|calorie| calorie.parse::<u32>().unwrap())
-                .collect::<Vec<u32>>()
-                .iter()
                 .sum::<u32>()
         })
         .collect::<Vec<u32>>()
