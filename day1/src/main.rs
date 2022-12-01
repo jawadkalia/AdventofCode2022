20ff6fb841296637b2c47cec2c76784b3a17be1e
Jawad Kalia <>
2022-12-01 16:20:50 -0600

    day1b done

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,10 +1,23 @@
+use std::collections::BinaryHeap;
+
 fn main() {
-    let value = parse_into_vector_of_sums()
-        .iter()
-        .max()
-        .unwrap()
-        .to_owned();
-    println!("Day 1a answer is {:#?}", value);
+    // Parse the input into an array of summed integers
+    let parsed_value = parse_into_vector_of_sums();
+
+    // Calculate day1_a answer
+    let day1_a = &parsed_value.iter().max().unwrap().to_owned();
+    
+    // Calculate day2_a answer
+    let mut vec_of_3 = Vec::new();
+
+    let mut parsed_binary_heap = BinaryHeap::from(parsed_value);
+    for _i in 0..3 {
+        vec_of_3.push(parsed_binary_heap.pop().unwrap());
+    }
+
+    println!("Day 1a answer is {:#?}", day1_a);
+    println!("Day 1b answer is {}", vec_of_3.iter().sum::<u32>());
+    println!("Day 1b answer is {:#?}", vec_of_3);
 }
 
 fn parse_into_vector_of_sums() -> Vec<u32> {
