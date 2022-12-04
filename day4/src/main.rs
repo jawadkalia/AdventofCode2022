0aac464fc5a2f1a28efc03741cb8fdd3570ca19d
AhyaanTech <ahyaantech@gmail.com>
2022-12-04 01:47:35 -0600

    k calling it

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,3 +1,35 @@
+use std::{collections::HashSet, vec};
+
 fn main() {
-    println!("Hello, world!");
+    let mut count = 0;
+
+    let data = include_str!("../input.txt")
+        .lines()
+        .map(|f| {
+            f.split(',')
+                .map(|s| {
+                    s.split('-')
+                        .map(|f| f.parse::<usize>().unwrap())
+                        .collect::<Vec<usize>>()
+                })
+                .collect::<Vec<Vec<usize>>>()
+        })
+        .map(two_sized_vector_to_tuple)
+        .for_each(|((a, b), (c, d))| {
+            if (a <= c && b >= d) || (c <= a && d >= b) {
+                println!("found");
+                count += 1;
+            } else {
+                println!("not found");
+            }
+        });
+
+    dbg!(data);
+}
+
+pub fn two_sized_vector_to_tuple(data: Vec<Vec<usize>>) -> Vec<(usize, usize)> {
+    let mut value = Vec::new();
+    data.iter()
+        .for_each(|f| value.push((f.first(), f.get(1).unwrap())));
+    value
 }
